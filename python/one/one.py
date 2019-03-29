d7517fd62c6a1da8dffbeca150ca0f913ab42f82
Nolan Aguirre <nolanaguirre08@gmail.com>
2019-03-29 17:25:19 -0500

    used defualt random names

--- python/one/one.py
+++ python/one/one.py
@@ -1,10 +1,10 @@
-from rectangle import Rectangle 
-import random as rand
+from rectangle import Rectangle
+import random
 rectangles = []
 
 for x in range(10):
-    rect_x = x * rand.randint(1,10) + 1
-    rect_y = x * rand.randint(1,10) + 1
+    rect_x = x * random.randint(1,10) + 1
+    rect_y = x * random.randint(1,10) + 1
     rectangles.append(Rectangle(rect_x, rect_y))
 
 for rectangle in rectangles:
